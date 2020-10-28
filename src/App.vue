f1aac5644b8d9533c4c6e484f7961fa93594adb7
yodaRoot <gonzalolopezguerrero2050@gmail.com>
2020-10-28 14:53:11 -0500

    view navbar and table v1

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <main >
+    <px-header />
+    <px-assets-table />
+  </main>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import PxHeader from "@/components/PxHeader";
+import PxAssetsTable from "@/components/PxAssetsTable";
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
+  name: "app",
+  components: { PxHeader, PxAssetsTable }
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
